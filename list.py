10fc48b20452bc4f8cd54f54abd1ae88c8cdcc93
AdarshMungra <adarshmungra1@gmail.com>
2022-09-07 11:09:00 +0400

    ..

---
+++ list.py
@@ -0,0 +1,19 @@
+
+
+
+
+bicycle =['trek', 'connondale','redline']
+print(bicycle)
+
+for i in bicycle:
+    print(i)
+
+for value in range(1,5):
+    print(value)
+
+
+#tuples
+dimensions = (200, 50)
+
+for dimensions in dimensions:
+    print(dimensions)
